c423ef050f36fa7f9f762e7064d6dfc135cd9d12
Matas <matas.mikusauskas@gmail.com>
2020-05-16 16:43:38 +0300

    added mail sender

--- product_scraper.rb
+++ product_scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require_relative 'send_mail'
 
 class ProductScraper
 
@@ -8,7 +9,7 @@ class ProductScraper
     @product_data = {}
     @category_data = category_data
     @file = File.open("configs/#{file_name}.txt")
-    @data = eval(@file.read)
+    @config_data = eval(@file.read)
     @file.close
     self.parse_data
   end
@@ -16,21 +17,20 @@ class ProductScraper
   def parse_data
     (0...@category_data.size).each do |index|
       product_scraper = Nokogiri::HTML(open(@category_data[index][:product_link]))
-      name = product_scraper.xpath(@data[:product_scrape][:name_selector]).text.split(" ").join(" ")
-      city = product_scraper.xpath(@data[:product_scrape][:city_selector]).text.split(" ").join(" ")
-      condition = product_scraper.xpath(@data[:product_scrape][:condition_selector]).text.split(" ").join(" ")
-      description = product_scraper.xpath(@data[:product_scrape][:description_selector]).text.split(" ").join(" ")
-      seller_number = product_scraper.xpath(@data[:product_scrape][:seller_number_selector]).text.split(" ").join(" ")
-      product_price = product_scraper.xpath(@data[:product_scrape][:product_price]).text.split(" ").join(" ")
+      name = product_scraper.xpath(@config_data[:product_scrape][:name_selector]).text.split(" ").join(" ")
+      city = product_scraper.xpath(@config_data[:product_scrape][:city_selector]).text.split(" ").join(" ")
+      condition = product_scraper.xpath(@config_data[:product_scrape][:condition_selector]).text.split(" ").join(" ")
+      description = product_scraper.xpath(@config_data[:product_scrape][:description_selector]).text.split(" ").join(" ")
+      seller_number = product_scraper.xpath(@config_data[:product_scrape][:seller_number_selector]).text.split(" ").join(" ")
+      product_price = product_scraper.xpath(@config_data[:product_scrape][:product_price]).text.split(" ").join(" ")
 
-      @product_data.store(index, {name: name, city: city, condition: condition, description: description, seller_number: seller_number, product_price: product_price})
+      @product_data.store(index, {name: name, city: city, condition: condition, description: description, seller_number: seller_number, product_price: product_price, product_link: @category_data[index][:product_link]})
     end
-    self.get_data
+    self.send_data_to_mail_sender
   end
 
-  def get_data
-    @product_data.each_key do |index|
-      puts @product_data[index]
-    end
+  def send_data_to_mail_sender
+    send_email = MailSender.new(@config_data, @product_data)
+    send_email.detailed_mail
   end
 end
--- send_mail.rb
+++ send_mail.rb
@@ -1,1 +1,62 @@
+require 'sendgrid-ruby'
+include SendGrid
+require 'dotenv'
 
+class MailSender
+
+  def initialize(config_data, product_info)
+    Dotenv.load('sendgrid.env')
+    @config_data = config_data
+    @product_info = product_info
+  end
+
+  def create_html_email_content
+    html_content = "<h3>Hello!</h3><br>Product(-s) I have found:"
+    if @config_data[:detailed_information_about_product]
+      (0...@product_info.size).each do |index|
+        html_content += "<br><br><b>Product nr. #{index+1}:</b>
+                        <br><b>Name:</b> #{@product_info[index][:name]},
+                        <br><b>City:</b> #{@product_info[index][:city]},
+                        <br><b>Condition:</b> #{@product_info[index][:condition]},
+                        <br><b>Description:</b> #{@product_info[index][:description]},
+                        <br><b>Price:</b> #{@product_info[index][:product_price]},
+                        <br><b>Seller number:</b> #{@product_info[index][:seller_number]},
+                        <br><b>Product link:</b> <a href=\"#{@product_info[index][:product_link]}\">Click</a>"
+      end
+    else
+      (0...@product_info.size).each do |index|
+        html_content += "<br><br><b>Product nr. #{index+1}:</b>
+                        <br><b>Name:</b> #{@product_info[index][:name]},
+                        <br><b>City:</b> #{@product_info[index][:city]},
+                        <br><b>Price:</b> #{@product_info[index][:price]},
+                        <br><b>Product link:</b> <a href=\"#{@product_info[index][:product_link]}\">Click</a>"
+      end
+    end
+    html_content
+  end
+
+  def standard_mail
+    from = SendGrid::Email.new(email: 'scraperruby@gmail.com')
+    to = SendGrid::Email.new(email: ENV['MY_EMAIL'])
+    subject = 'I have found something that you might like'
+    content = SendGrid::Content.new(type: 'text/html', value: self.create_html_email_content)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    self.send_mail(mail)
+  end
+
+  def detailed_mail
+    from = SendGrid::Email.new(email: 'scraperruby@gmail.com')
+    to = SendGrid::Email.new(email: ENV['MY_EMAIL'])
+    subject = 'I have found something that you might like'
+    content = SendGrid::Content.new(type: 'text/html', value: self.create_html_email_content)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    self.send_mail(mail)
+  end
+
+  def send_mail(mail)
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+end
